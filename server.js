71dcb1cf8185183460782a66508a16d1b3008896
Kevin Tursi <kevin.tursi@uconn.edu>
2023-04-25 18:28:42 -0400

    Added knex to GET user profile endpoint

--- server.js
+++ server.js
@@ -83,16 +83,16 @@ app.post('/register', (req, res) => {
 // GET user by profile
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    knex.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('Not found')
         }
+        
     })
-    if (!found) {
-        res.status(400).json('user not found')
-    }
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 // IMAGE to increase entries count
